35bcebf9583502f64709c7bc4ce03ca90dcb9a16
burdolski <98654643+burdolski@users.noreply.github.com>
2022-06-09 00:23:56 -0600

    Create Objective3Lab2.java

---
+++ Objective3Lab2.java
@@ -0,0 +1,8 @@
+public static void main(String[] args){
+    int num1 = 10;
+    int num2 = 8;
+      double num1 = Math.sqrt(10*10);
+      double num2 = Math.sqrt(8*8);
+      double result = Math.sqrt(10) + Math.sqrt(8);
+  }
+}
